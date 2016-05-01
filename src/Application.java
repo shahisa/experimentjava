32be4cdd2c96b048dd8db06557b78bd1db92eff9
Isaiah Weaver <iswea1@morgan.edu>
2016-05-01 13:34:18 -0400

    experiment addid basic java files

---
+++ src/Application.java
@@ -0,0 +1,30 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int myNumber = 79;
+		short myShort = 382;
+		long myLong = 808230725;
+		double myDouble = 8972.80732;
+		float myFloat = 980834.47f;
+		
+		char myChar = 'y';
+		boolean myBool = true;
+		
+		byte myByte = 127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myByte);
+		System.out.println(myBool);
+		
+	}
+
+}
